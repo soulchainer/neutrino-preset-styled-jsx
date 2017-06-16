77a7cd4c7679f889cdfd403240661f3153974bf9
Juan Riquelme González <juanriqgon@protonmail.com>
2017-06-17 00:16:53 +0200

    test: add basic tests & little change on index
    They're basically the same basic tests used in some Neutrino base presets, but adapted to use `jest`
    instead of `ava`. The tests made necessary/revealed that the preset should have a direct dependency
    on `neutrino-preset-react`. After all, without it installed, the preset couldn't work, neither be
    correctly tested.
    
    BREAKING CHANGE: Added a dependency on neutrino-preset-react

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const loaderMerge = require('neutrino-middleware-loader-merge');
+const react = require('neutrino-preset-react');
 
 module.exports = (neutrino, options) => {
+  neutrino.use(react);
   neutrino.use(loaderMerge('compile', 'babel'), {
     plugins: ['styled-jsx/babel']
   });
